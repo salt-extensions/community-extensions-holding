saltstack/salt

Issue 14304: add proxy parameter to gem state

salt/modules/gem.py
@@ -36,7 +36,8 @@ def install(gems,           # pylint: disable=C0103
             version=None,
             rdoc=False,
             ri=False,
-            pre_releases=False):      # pylint: disable=C0103
+            pre_releases=False,
+            proxy=None):      # pylint: disable=C0103
     '''
     Installs one or several gems.
 
@@ -55,6 +56,9 @@ def install(gems,           # pylint: disable=C0103
         Generate RI documentation for the gem(s).
     pre_releases
         Include pre-releases in the available versions
+    proxy : None
+        Use the specified HTTP proxy server for all outgoing traffic.
+        Format: http://hostname[:port]
 
     CLI Example:
 
@@ -71,6 +75,8 @@ def install(gems,           # pylint: disable=C0103
         options.append('--no-ri')
     if pre_releases:
         options.append('--pre')
+    if proxy:
+        options.append('-p {0}'.format(proxy))
 
     cmdline_args = ' '.join(options)
     return _gem('install {gems} {options}'.format(gems=gems,

salt-extensions/community-extensions-holding

salt/modules/gem.py
@@ -35,7 +35,8 @@ def install(gems,           # pylint: disable=C0103
             runas=None,
             version=None,
             rdoc=False,
-            ri=False):      # pylint: disable=C0103
+            ri=False,
+            proxy=None):      # pylint: disable=C0103
     '''
     Installs one or several gems.
 
@@ -52,6 +53,9 @@ def install(gems,           # pylint: disable=C0103
         Generate RDoc documentation for the gem(s).
     ri : False
         Generate RI documentation for the gem(s).
+    proxy : None
+        Use the specified HTTP proxy server for all outgoing traffic.
+        Format: http://hostname[:port]
 
     CLI Example:
 
@@ -66,6 +70,8 @@ def install(gems,           # pylint: disable=C0103
         options += ' --no-rdoc'
     if not ri:
         options += ' --no-ri'
+    if proxy:
+        options += '-p {0}'.format(proxy)
 
     return _gem('install {gems} {options}'.format(gems=gems, options=options),
                 ruby,