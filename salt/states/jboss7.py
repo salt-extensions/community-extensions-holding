saltstack/salt

Py3 compatibility fixes. Dos 2 Unix line breaks. Lint.

salt/states/jboss7.py
@@ -73,13 +73,19 @@ For the sake of brevity, examples for each state assume that jboss_config is mov
 
 '''
 
+# Import python libs
+from __future__ import absolute_import
 import time
 import logging
 import re
 import traceback
-from salt.utils import dictdiffer
 
+# Import Salt libs
+from salt.utils import dictdiffer
 from salt.exceptions import CommandExecutionError
+
+# Import 3rd-party libs
+import salt.ext.six as six
 
 log = logging.getLogger(__name__)
 
@@ -218,14 +224,14 @@ def __format_ds_changes(keys, old_dict, new_dict):
     return changes
 
 
-def __get_ds_value(dict, key):
-    log.debug("__get_value(dict,%s)", key)
+def __get_ds_value(_dict, key):
+    log.debug("__get_value(dict, {0})".format(key))
     if key == "password":
         return "***"
-    elif dict[key] is None:
+    elif _dict[key] is None:
         return 'undefined'
     else:
-        return str(dict[key])
+        return str(_dict[key])
 
 
 def bindings_exist(name, jboss_config, bindings):
@@ -287,13 +293,13 @@ def bindings_exist(name, jboss_config, bindings):
     return ret
 
 
-def __log_binding_change(changes, type, key, new, old=None):
-    if type not in changes:
-        changes[type] = ''
+def __log_binding_change(changes, _type, key, new, old=None):
+    if _type not in changes:
+        changes[_type] = ''
     if old is None:
-        changes[type] += key + ':' + new + '\n'
+        changes[_type] += key + ':' + new + '\n'
     else:
-        changes[type] += key + ':' + old + '->' + new + '\n'
+        changes[_type] += key + ':' + old + '->' + new + '\n'
 
 
 def deployed(name, jboss_config, artifact=None, salt_source=None):
@@ -611,21 +617,21 @@ def __fetch_from_artifactory(artifact):
 
     if 'latest_snapshot' in artifact and artifact['latest_snapshot']:
         fetch_result = __salt__['artifactory.get_latest_snapshot'](artifactory_url=artifact['artifactory_url'],
-                                                                      repository=artifact['repository'],
-                                                                      group_id=artifact['group_id'],
-                                                                      artifact_id=artifact['artifact_id'],
-                                                                      packaging=artifact['packaging'],
-                                                                      target_dir=target_dir)
+                                                                   repository=artifact['repository'],
+                                                                   group_id=artifact['group_id'],
+                                                                   artifact_id=artifact['artifact_id'],
+                                                                   packaging=artifact['packaging'],
+                                                                   target_dir=target_dir)
     elif str(artifact['version']).endswith('SNAPSHOT'):
         if 'snapshot_version' in artifact:
             fetch_result = __salt__['artifactory.get_snapshot'](artifactory_url=artifact['artifactory_url'],
-                                                                       repository=artifact['repository'],
-                                                                       group_id=artifact['group_id'],
-                                                                       artifact_id=artifact['artifact_id'],
-                                                                       packaging=artifact['packaging'],
-                                                                       version=artifact['version'],
-                                                                       snapshot_version=artifact['snapshot_version'],
-                                                                       target_dir=target_dir)
+                                                                repository=artifact['repository'],
+                                                                group_id=artifact['group_id'],
+                                                                artifact_id=artifact['artifact_id'],
+                                                                packaging=artifact['packaging'],
+                                                                version=artifact['version'],
+                                                                snapshot_version=artifact['snapshot_version'],
+                                                                target_dir=target_dir)
         else:
             fetch_result = __salt__['artifactory.get_snapshot'](artifactory_url=artifact['artifactory_url'],
                                                                 repository=artifact['repository'],
@@ -636,12 +642,12 @@ def __fetch_from_artifactory(artifact):
                                                                 target_dir=target_dir)
     else:
         fetch_result = __salt__['artifactory.get_release'](artifactory_url=artifact['artifactory_url'],
-                                                              repository=artifact['repository'],
-                                                              group_id=artifact['group_id'],
-                                                              artifact_id=artifact['artifact_id'],
-                                                              packaging=artifact['packaging'],
-                                                              version=artifact['version'],
-                                                              target_dir=target_dir)
+                                                           repository=artifact['repository'],
+                                                           group_id=artifact['group_id'],
+                                                           artifact_id=artifact['artifact_id'],
+                                                           packaging=artifact['packaging'],
+                                                           version=artifact['version'],
+                                                           target_dir=target_dir)
     return fetch_result
 
 
@@ -713,9 +719,9 @@ def reloaded(name, jboss_config, timeout=60, interval=5):
     return ret
 
 
-def __check_dict_contains(dict, dict_name, keys, comment='', result=True):
+def __check_dict_contains(_dict, dict_name, keys, comment='', result=True):
     for key in keys:
-        if key not in dict.keys():
+        if key not in six.iterkeys(_dict):
             result = False
             comment = __append_comment("Missing {0} in {1}".format(key, dict_name), comment)
     return result, comment

salt-extensions/community-extensions-holding

salt/states/jboss7.py
@@ -73,13 +73,19 @@ For the sake of brevity, examples for each state assume that jboss_config is mov
 
 '''
 
+# Import python libs
+from __future__ import absolute_import
 import time
 import logging
 import re
 import traceback
-from salt.utils import dictdiffer
 
+# Import Salt libs
+from salt.utils import dictdiffer
 from salt.exceptions import CommandExecutionError
+
+# Import 3rd-party libs
+import salt.ext.six as six
 
 log = logging.getLogger(__name__)
 
@@ -611,21 +617,21 @@ def __fetch_from_artifactory(artifact):
 
     if 'latest_snapshot' in artifact and artifact['latest_snapshot']:
         fetch_result = __salt__['artifactory.get_latest_snapshot'](artifactory_url=artifact['artifactory_url'],
-                                                                      repository=artifact['repository'],
-                                                                      group_id=artifact['group_id'],
-                                                                      artifact_id=artifact['artifact_id'],
-                                                                      packaging=artifact['packaging'],
-                                                                      target_dir=target_dir)
+                                                                   repository=artifact['repository'],
+                                                                   group_id=artifact['group_id'],
+                                                                   artifact_id=artifact['artifact_id'],
+                                                                   packaging=artifact['packaging'],
+                                                                   target_dir=target_dir)
     elif str(artifact['version']).endswith('SNAPSHOT'):
         if 'snapshot_version' in artifact:
             fetch_result = __salt__['artifactory.get_snapshot'](artifactory_url=artifact['artifactory_url'],
-                                                                       repository=artifact['repository'],
-                                                                       group_id=artifact['group_id'],
-                                                                       artifact_id=artifact['artifact_id'],
-                                                                       packaging=artifact['packaging'],
-                                                                       version=artifact['version'],
-                                                                       snapshot_version=artifact['snapshot_version'],
-                                                                       target_dir=target_dir)
+                                                                repository=artifact['repository'],
+                                                                group_id=artifact['group_id'],
+                                                                artifact_id=artifact['artifact_id'],
+                                                                packaging=artifact['packaging'],
+                                                                version=artifact['version'],
+                                                                snapshot_version=artifact['snapshot_version'],
+                                                                target_dir=target_dir)
         else:
             fetch_result = __salt__['artifactory.get_snapshot'](artifactory_url=artifact['artifactory_url'],
                                                                 repository=artifact['repository'],
@@ -636,12 +642,12 @@ def __fetch_from_artifactory(artifact):
                                                                 target_dir=target_dir)
     else:
         fetch_result = __salt__['artifactory.get_release'](artifactory_url=artifact['artifactory_url'],
-                                                              repository=artifact['repository'],
-                                                              group_id=artifact['group_id'],
-                                                              artifact_id=artifact['artifact_id'],
-                                                              packaging=artifact['packaging'],
-                                                              version=artifact['version'],
-                                                              target_dir=target_dir)
+                                                           repository=artifact['repository'],
+                                                           group_id=artifact['group_id'],
+                                                           artifact_id=artifact['artifact_id'],
+                                                           packaging=artifact['packaging'],
+                                                           version=artifact['version'],
+                                                           target_dir=target_dir)
     return fetch_result
 
 
@@ -715,7 +721,7 @@ def reloaded(name, jboss_config, timeout=60, interval=5):
 
 def __check_dict_contains(dct, dict_name, keys, comment='', result=True):
     for key in keys:
-        if key not in dct.keys():
+        if key not in six.iterkeys(dct):
             result = False
             comment = __append_comment("Missing {0} in {1}".format(key, dict_name), comment)
     return result, comment