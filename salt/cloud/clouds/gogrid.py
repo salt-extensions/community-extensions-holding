salt-extensions/community-extensions-holding

commit 429695c7bd3c9e9b80cce59454c94980fdcf5675

Code clean up of cloud drivers and files
Sonar Cube!

salt/cloud/clouds/gogrid.py
@@ -220,7 +220,7 @@ def list_nodes(full=False, call=None):
 
     for node in nodes:
         ret[node] = {}
-        for item in ('id', 'image', 'size', 'public_ips', 'private_ips', 'state'):
+        for item in 'id', 'image', 'size', 'public_ips', 'private_ips', 'state':
             ret[node][item] = nodes[node][item]
 
     return ret

salt/cloud/clouds/ec2.py
@@ -1177,10 +1177,9 @@ def _create_eni_if_necessary(interface):
         if 'item' in subnet_query_result:
             if isinstance(subnet_query_result['item'], dict):
                 for key, value in subnet_query_result['item'].iteritems():
-                    if key == "subnetId":
-                        if value == interface['SubnetId']:
-                            found = True
-                            break
+                    if key == "subnetId" and value == interface['SubnetId']:
+                        found = True
+                        break
             else:
                 for subnet in subnet_query_result['item']:
                     if subnet['subnetId'] == interface['SubnetId']:
@@ -1651,11 +1650,10 @@ def request_instance(vm_=None, call=None):
         'del_root_vol_on_destroy', vm_, __opts__, search_global=False
     )
 
-    if set_del_root_vol_on_destroy is not None:
-        if not isinstance(set_del_root_vol_on_destroy, bool):
-            raise SaltCloudConfigError(
-                '\'del_root_vol_on_destroy\' should be a boolean value.'
-            )
+    if set_del_root_vol_on_destroy and not isinstance(set_del_root_vol_on_destroy, bool):
+        raise SaltCloudConfigError(
+            '\'del_root_vol_on_destroy\' should be a boolean value.'
+        )
 
     vm_['set_del_root_vol_on_destroy'] = set_del_root_vol_on_destroy
 
@@ -1737,11 +1735,10 @@ def request_instance(vm_=None, call=None):
         'del_all_vols_on_destroy', vm_, __opts__, search_global=False, default=False
     )
 
-    if set_del_all_vols_on_destroy is not None:
-        if not isinstance(set_del_all_vols_on_destroy, bool):
-            raise SaltCloudConfigError(
-                '\'del_all_vols_on_destroy\' should be a boolean value.'
-            )
+    if set_del_all_vols_on_destroy and not isinstance(set_del_all_vols_on_destroy, bool):
+        raise SaltCloudConfigError(
+            '\'del_all_vols_on_destroy\' should be a boolean value.'
+        )
 
     salt.utils.cloud.fire_event(
         'event',

salt/cloud/clouds/gce.py
@@ -1580,32 +1580,36 @@ def create_disk(kwargs=None, call=None):
             'The create_disk function must be called with -f or --function.'
         )
 
-    if not kwargs or 'location' not in kwargs:
+    if kwargs is None:
+        kwargs = {}
+
+    name = kwargs.get('disk_name', None)
+    image = kwargs.get('image', None)
+    location = kwargs.get('location', None)
+    size = kwargs.get('size', None)
+    snapshot = kwargs.get('snapshot', None)
+
+    if location is None:
         log.error(
             'A location (zone) must be specified when creating a disk.'
         )
         return False
 
-    if 'disk_name' not in kwargs:
+    if name is None:
         log.error(
             'A disk_name must be specified when creating a disk.'
         )
         return False
 
-    if 'size' not in kwargs:
-        if 'image' not in kwargs and 'snapshot' not in kwargs:
-            log.error(
-                'Must specify image, snapshot, or size.'
-            )
-            return False
+    if 'size' is None and ('image' is None and 'snapshot' is None):
+        log.error(
+            'Must specify image, snapshot, or size.'
+        )
+        return False
 
     conn = get_conn()
 
-    size = kwargs.get('size', None)
-    name = kwargs.get('disk_name')
     location = conn.ex_get_zone(kwargs['location'])
-    snapshot = kwargs.get('snapshot', None)
-    image = kwargs.get('image', None)
     use_existing = True
 
     salt.utils.cloud.fire_event(
@@ -1960,9 +1964,9 @@ def destroy(vm_name, call=None):
                 profile = md['value']
     vm_ = get_configured_provider()
     delete_boot_pd = False
-    if profile is not None and profile in vm_['profiles']:
-        if 'delete_boot_pd' in vm_['profiles'][profile]:
-            delete_boot_pd = vm_['profiles'][profile]['delete_boot_pd']
+
+    if profile and profile in vm_['profiles'] and 'delete_boot_pd' in vm_['profiles'][profile]:
+        delete_boot_pd = vm_['profiles'][profile]['delete_boot_pd']
 
     try:
         inst_deleted = conn.destroy_node(node)

salt/cloud/clouds/linode.py
@@ -1362,9 +1362,8 @@ def _query(action=None,
     if 'api_key' not in args.keys():
         args['api_key'] = apikey
 
-    if action:
-        if 'api_action' not in args.keys():
-            args['api_action'] = '{0}.{1}'.format(action, command)
+    if action and 'api_action' not in args.keys():
+        args['api_action'] = '{0}.{1}'.format(action, command)
 
     if header_dict is None:
         header_dict = {}
@@ -1421,9 +1420,8 @@ def _wait_for_job(linode_id, job_id, timeout=300, quiet=True):
         jobs_result = _query('linode',
                              'job.list',
                              args={'LinodeID': linode_id})['DATA']
-        if jobs_result[0]['JOBID'] == job_id:
-            if jobs_result[0]['HOST_SUCCESS'] == 1:
-                return True
+        if jobs_result[0]['JOBID'] == job_id and jobs_result[0]['HOST_SUCCESS'] == 1:
+            return True
 
         time.sleep(interval)
         if not quiet:

salt/cloud/clouds/openstack.py
@@ -707,10 +707,9 @@ def create(vm_):
                     if private_ip not in data.private_ips and not ignore_ip:
                         result.append(private_ip)
 
-        if rackconnect(vm_) is True:
-            if ssh_interface(vm_) != 'private_ips':
-                data.public_ips = access_ip
-                return data
+        if rackconnect(vm_) is True and ssh_interface(vm_) != 'private_ips':
+            data.public_ips = access_ip
+            return data
 
         # populate return data with private_ips
         # when ssh_interface is set to private_ips and public_ips exist

salt/cloud/clouds/softlayer.py
@@ -414,9 +414,10 @@ def create(vm_):
         '''
         node_info = pass_conn.getVirtualGuests(id=response['id'], mask=mask)
         for node in node_info:
-            if node['id'] == response['id']:
-                if 'passwords' in node['operatingSystem'] and len(node['operatingSystem']['passwords']) > 0:
-                    return node['operatingSystem']['passwords'][0]['username'], node['operatingSystem']['passwords'][0]['password']
+            if node['id'] == response['id'] and \
+                            'passwords' in node['operatingSystem'] and \
+                            len(node['operatingSystem']['passwords']) > 0:
+                return node['operatingSystem']['passwords'][0]['username'], node['operatingSystem']['passwords'][0]['password']
         time.sleep(5)
         return False
 

salt/cloud/clouds/softlayer_hw.py
@@ -372,9 +372,10 @@ def create(vm_):
         '''
         node_info = pass_conn.getVirtualGuests(id=response['id'], mask=mask)
         for node in node_info:
-            if node['id'] == response['id']:
-                if 'passwords' in node['operatingSystem'] and len(node['operatingSystem']['passwords']) > 0:
-                    return node['operatingSystem']['passwords'][0]['password']
+            if node['id'] == response['id'] \
+                    and 'passwords' in node['operatingSystem'] \
+                    and len(node['operatingSystem']['passwords']) > 0:
+                return node['operatingSystem']['passwords'][0]['password']
         time.sleep(5)
         return False
 

salt/cloud/clouds/vmware.py
@@ -103,6 +103,8 @@ except ImportError:
     except ImportError:
         HAS_SIX = False
 
+IP_RE = r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+
 # Get logging started
 log = logging.getLogger(__name__)
 
@@ -783,15 +785,14 @@ def _wait_for_ip(vm_ref, max_wait):
         if time_counter % 5 == 0:
             log.info("[ {0} ] Waiting to retrieve IPv4 information [{1} s]".format(vm_ref.name, time_counter))
 
-        if vm_ref.summary.guest.ipAddress:
-            if match(r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', vm_ref.summary.guest.ipAddress) and vm_ref.summary.guest.ipAddress != '127.0.0.1':
-                log.info("[ {0} ] Successfully retrieved IPv4 information in {1} seconds".format(vm_ref.name, time_counter))
-                return vm_ref.summary.guest.ipAddress
+        if vm_ref.summary.guest.ipAddress and match(IP_RE, vm_ref.summary.guest.ipAddress) and vm_ref.summary.guest.ipAddress != '127.0.0.1':
+            log.info("[ {0} ] Successfully retrieved IPv4 information in {1} seconds".format(vm_ref.name, time_counter))
+            return vm_ref.summary.guest.ipAddress
 
         for net in vm_ref.guest.net:
             if net.ipConfig.ipAddress:
                 for current_ip in net.ipConfig.ipAddress:
-                    if match(r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', current_ip.ipAddress) and current_ip.ipAddress != '127.0.0.1':
+                    if match(IP_RE, current_ip.ipAddress) and current_ip.ipAddress != '127.0.0.1':
                         log.info("[ {0} ] Successfully retrieved IPv4 information in {1} seconds".format(vm_ref.name, time_counter))
                         return current_ip.ipAddress
         time.sleep(1.0 - ((time.time() - starttime) % 1.0))
@@ -2381,24 +2382,24 @@ def create(vm_):
             config=config_spec
         )
 
-        if devices and 'network' in list(devices.keys()):
-            if "Windows" not in object_ref.config.guestFullName:
-                global_ip = vim.vm.customization.GlobalIPSettings()
-                if 'dns_servers' in list(vm_.keys()):
-                    global_ip.dnsServerList = vm_['dns_servers']
+        if devices and 'network' in list(devices.keys()) and 'Windows' not in object_ref.config.guestFullName:
+            global_ip = vim.vm.customization.GlobalIPSettings()
 
-                identity = vim.vm.customization.LinuxPrep()
-                hostName = vm_name.split('.')[0]
-                domainName = vm_name.split('.', 1)[-1]
-                identity.hostName = vim.vm.customization.FixedName(name=hostName)
-                identity.domain = domainName if hostName != domainName else domain
+            if 'dns_servers' in list(vm_.keys()):
+                global_ip.dnsServerList = vm_['dns_servers']
 
-                custom_spec = vim.vm.customization.Specification(
-                    globalIPSettings=global_ip,
-                    identity=identity,
-                    nicSettingMap=specs['nics_map']
-                )
-                clone_spec.customization = custom_spec
+            identity = vim.vm.customization.LinuxPrep()
+            hostName = vm_name.split('.')[0]
+            domainName = vm_name.split('.', 1)[-1]
+            identity.hostName = vim.vm.customization.FixedName(name=hostName)
+            identity.domain = domainName if hostName != domainName else domain
+
+            custom_spec = vim.vm.customization.Specification(
+                globalIPSettings=global_ip,
+                identity=identity,
+                nicSettingMap=specs['nics_map']
+            )
+            clone_spec.customization = custom_spec
 
         if not template:
             clone_spec.powerOn = power

commit 48d0986d73b0d635442ab2d9d6574893b192bf5e

Fix bug introduced in 429695c7bd3c9e9b80cce59454c94980fdcf5675
During the cleanup the tuple was made into... nothing that python can interpret

salt/cloud/clouds/gogrid.py
@@ -220,7 +220,7 @@ def list_nodes(full=False, call=None):
 
     for node in nodes:
         ret[node] = {}
-        for item in 'id', 'image', 'size', 'public_ips', 'private_ips', 'state':
+        for item in ('id', 'image', 'size', 'public_ips', 'private_ips', 'state'):
             ret[node][item] = nodes[node][item]
 
     return ret