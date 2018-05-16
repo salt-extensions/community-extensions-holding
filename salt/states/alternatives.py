salt-extensions/community-extensions-holding

commit ef41f9572563f6c913f561a34700c979cf42f074

Fix alternative module and state.

salt/states/alternatives.py
@@ -62,7 +62,7 @@ def install(name, link, path, priority):
            'changes': {},
            'comment': ''}
 
-    isinstalled = __salt__['alternatives.check_exists'](name, path)
+    isinstalled = __salt__['alternatives.check_installed'](name, path)
     if not isinstalled:
         if __opts__['test']:
             ret['comment'] = (

commit f58641bf707302e9d3e0c323a2a2ddb6d7cbfaf8

fix states.alternatives.install to really install alternative
The previous logic confused installing a new alternative and activating
that new alternative as the default link for that link group (this might
have been helped by the slightly confusing terminology here -
modules.alternatives.check_installed() checks whether the given path is
the dault link, but states.alternatives.install() should just install
another alternative). That logic failed if we installed a new alternative
with lower priority than the current default link (or with the link
group in manual mode): the default link does not change and the state
assumed that the installation of the alternative failed. Part of this
breakage resulted from commit ef41f9572563f6c913f561a34700c979cf42f074 .

This change puts the logic back on it's feet:
- Case 1 is when the alternative is already installed (that is, registered
  to the system as an alternative in that link group) as checked per
  modules.alternatives.check_exists(). No change there, and we just
  set a comment to that effect. It's open to debate if we should handle
  priority changes here.
- Case 2 registers the new alternative, and following that the default
  of that link group changes. That's a success, and we log a comment
  to that effect.
- Case 3 is when the link has to be installed, but the default does not
  change (link group in "manual" mode or lower priority). That too is
  to be considered a success - the new alternative can still become the
  default by either manual action or when the higher-priority alternative
  is removed later.
- Case 4 is when the alternative installation fails for some reason: we
  can detect that if modules.alternatives.check_exists() still returns
  False after link installation. That's a failure, and thus we set
  "result" to False with some comment.

Fixes: #38802
Fixes: #39417

salt/states/alternatives.py
@@ -70,9 +70,8 @@ def install(name, link, path, priority):
            'changes': {},
            'comment': ''}
 
-    isinstalled = __salt__['alternatives.check_installed'](name, path)
-    if isinstalled:
-        ret['comment'] = 'Alternatives for {0} is already set to {1}'.format(name, path)
+    if __salt__['alternatives.check_exists'](name, path):
+        ret['comment'] = 'Alternative {0} for {1} is already registered'.format(path, name)
     else:
         if __opts__['test']:
             ret['comment'] = (
@@ -82,12 +81,15 @@ def install(name, link, path, priority):
             return ret
 
         out = __salt__['alternatives.install'](name, link, path, priority)
-        current = __salt__['alternatives.show_current'](name)
-        master_link = __salt__['alternatives.show_link'](name)
-        if current == path and master_link == link:
-            ret['comment'] = (
-                'Alternative for {0} set to path {1} with priority {2}'
-            ).format(name, current, priority)
+        if __salt__['alternatives.check_exists'](name, path):
+            if __salt__['alternatives.check_installed'](name, path):
+                ret['comment'] = (
+                    'Alternative for {0} set to path {1} with priority {2}'
+                ).format(name, current, priority)
+            else:
+                ret['comment'] = (
+                    'Alternative {0} for {1} registered  with priority {2} and not set to default'
+                ).format(path, name, priority)
             ret['changes'] = {'name': name,
                               'link': link,
                               'path': path,