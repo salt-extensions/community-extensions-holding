salt-extensions/community-extensions-holding

commit 78e2a3efb4eb89563aea956e21fe225c4ceebfd3

values need to be bytes when writing to ldap

salt/modules/ldap3.py
@@ -402,18 +402,14 @@ def add(connect_spec, dn, attributes):
     # convert the "iterable of values" to lists in case that's what
     # addModlist() expects (also to ensure that the caller's objects
     # are not modified)
-    attributes = dict(((attr, list(vals))
+    attributes = dict(((attr, salt.utils.data.encode(list(vals)))
                        for attr, vals in six.iteritems(attributes)))
     log.info('adding entry: dn: %s attributes: %s', repr(dn), repr(attributes))
 
     if 'unicodePwd' in attributes:
         attributes['unicodePwd'] = [_format_unicode_password(x) for x in attributes['unicodePwd']]
 
-    modlist = salt.utils.data.decode(
-        ldap.modlist.addModlist(attributes),
-        to_str=True,
-        preserve_tuples=True
-    )
+    modlist = ldap.modlist.addModlist(attributes),
     try:
         l.c.add_s(dn, modlist)
     except ldap.LDAPError as e:

commit 9e6131160dcdb9edd5cbc198c241df5ad595071a

Fix ldap.managed errors
Regression from 78e2a3e (#48666):
- AttributeError: 'tuple' object has no attribute 'encode'
- ValueError: too many values to unpack

Fixes: #52022.

salt/modules/ldap3.py
@@ -409,7 +409,7 @@ def add(connect_spec, dn, attributes):
     if 'unicodePwd' in attributes:
         attributes['unicodePwd'] = [_format_unicode_password(x) for x in attributes['unicodePwd']]
 
-    modlist = ldap.modlist.addModlist(attributes),
+    modlist = ldap.modlist.addModlist(attributes)
     try:
         l.c.add_s(dn, modlist)
     except ldap.LDAPError as e: