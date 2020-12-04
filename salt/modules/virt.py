saltstack/salt

Add console and serial to update and running status

salt/modules/virt.py
@@ -143,6 +143,10 @@ VIRT_STATE_NAME_MAP = {
     6: "crashed",
 }
 
+default_port = 23023
+default_serial_type = "tcp"
+default_console_type = "tcp"
+
 
 def __virtual__():
     if not HAS_LIBVIRT:
@@ -780,6 +784,8 @@ def _gen_xml(
     graphics=None,
     boot=None,
     boot_dev=None,
+    serials=None,
+    consoles=None,
     **kwargs
 ):
     """
@@ -831,18 +837,76 @@ def _gen_xml(
                 context["boot"]["kernel"] = "/usr/lib/grub2/x86_64-xen/grub.xen"
                 context["boot_dev"] = []
 
-    if "serial_type" in kwargs:
-        context["serial_type"] = kwargs["serial_type"]
-    if "serial_type" in context and context["serial_type"] == "tcp":
-        if "telnet_port" in kwargs:
-            context["telnet_port"] = kwargs["telnet_port"]
-        else:
-            context["telnet_port"] = 23023  # FIXME: use random unused port
-    if "serial_type" in context:
-        if "console" in kwargs:
-            context["console"] = kwargs["console"]
-        else:
-            context["console"] = True
+    context["serials"] = []
+    if serials is not None:
+        for serial in serials:
+            serial_type = serial.get("type") or default_serial_type
+            if not serial_type:
+                raise SaltInvocationError("Missing type in serial")
+
+            serial_context = {
+                "type": serial_type
+            }
+            if serial_context["type"] == "tcp":
+                serial_context["port"] = serial.get("port", default_port)
+                serial_context["protocol"] = serial.get("protocol", "telnet")
+            context["serials"].append(serial_context)
+
+    context["consoles"] = []
+    if consoles is not None:
+        for console in consoles:
+            console_type = console.get("type") or default_console_type
+            if not console_type:
+                raise SaltInvocationError("Missing type in console")
+
+            console_context = {
+                "type": console_type
+            }
+            if console_context["type"] == "tcp":
+                console_context["port"] = console.get("port", default_port)
+                console_context["protocol"] = console.get("protocol", "telnet")
+            context["consoles"].append(console_context)
+
+    # processing of deprecated parameters
+    old_port = kwargs.get("telnet_port")
+    if old_port:
+        salt.utils.versions.warn_until(
+            "Aluminium",
+            "'telnet_port' parameter has been deprecated, use the 'serials' and 'consoles' parameters instead. "
+            "It will be removed in {version}.",
+        )
+
+    old_serial_type = kwargs.get("serial_type")
+    if old_serial_type:
+        salt.utils.versions.warn_until(
+            "Aluminium",
+            "'serial_type' parameter has been deprecated, use the 'serials' and 'consoles' parameters instead. "
+            "It will be removed in {version}.",
+        )
+        serial_context = {
+            "type": serial_type
+        }
+        if serial_context["type"] == "tcp":
+            serial_context["port"] = old_port if old_port is not None else default_port
+            serial_context["protocol"] = serial.get("protocol", "telnet")
+        context["serials"].append(serial_context)
+
+    old_console = kwargs.get("console")
+    if old_console:
+        salt.utils.versions.warn_until(
+            "Aluminium",
+            "'console' parameter has been deprecated, use the 'serials' and 'consoles' parameters instead. "
+            "It will be removed in {version}.",
+        )
+        if old_console is True:
+            console_context = {
+                "type": old_serial_type if old_serial_type else default_console_type
+            }
+            if console_context["type"] == "tcp":
+                console_context["port"] = old_port if old_port else default_port
+                console_context["protocol"] = console.get("protocol", "telnet")
+            context["consoles"].append(console_context)
+    # end processing of deprecated parameters
 
     context["disks"] = []
     disk_bus_map = {"virtio": "vd", "xen": "xvd", "fdc": "fd", "ide": "hd"}
@@ -1737,6 +1801,8 @@ def init(
     arch=None,
     boot=None,
     boot_dev=None,
+    serials=None,
+    consoles=None,
     **kwargs
 ):
     """
@@ -1856,6 +1922,17 @@ def init(
        A boolean value.
 
        .. versionadded:: sodium
+
+    :param serials:
+        Dictionary providing details on the serials connection to create. (Default: ``None``)
+        See :ref:`init-serials-def` for more details on the possible values.
+
+        .. versionadded:: Sodium
+    :param consoles:
+        Dictionary providing details on the consoles device to create. (Default: ``None``)
+        See :ref:`init-consoles-def` for more details on the possible values.
+
+        .. versionadded:: Sodium
 
     .. _init-nic-def:
 
@@ -2001,6 +2078,40 @@ def init(
 
         By default, not setting the ``listen`` part of the dictionary will default to
         listen on all addresses.
+
+    .. _init-serials-def:
+
+    .. rubric:: Serials Definitions
+
+    Serial dictionaries can contain the following properties:
+
+    type
+        Type of the serial connection, like ``'tcp'``, ``'pty'``.
+
+    port
+        The serial port number.
+        (Default: 23023)
+
+    Protocol
+        Name of connection protocol.
+        (Default: telnet)
+
+    .. _init-consoles-def:
+
+    .. rubric:: Consoles Definitions
+
+    Consol dictionaries can contain the following properties:
+
+    type
+        Type of the serial connection, like ``'tcp'``, ``'pty'``.
+
+    port
+        The serial port number.
+        (Default: 23023)
+
+    Protocol
+        Name of connection protocol.
+        (Default: telnet)
 
     .. rubric:: CLI Example
 
@@ -2151,6 +2262,8 @@ def init(
             graphics,
             boot,
             boot_dev,
+            serials,
+            consoles,
             **kwargs
         )
         log.debug("New virtual machine definition: %s", vm_xml)
@@ -2367,6 +2480,45 @@ def _diff_graphics_lists(old, new):
     return _diff_lists(old, new, _graphics_equal)
 
 
+def _serial_or_concole_equal(old, new):
+
+    def _filter_serial_or_concole(item):
+        """
+        Filter out elements to ignore when comparing items
+        """
+        return {
+            "type": item.attrib["type"],
+            "port": item.find("source").attrib["service"]
+            if item.find("source") is not None
+            else None,
+            "protocol": item.find("protocol").attrib["type"]
+            if item.find("protocol") is not None
+            else None,
+        }
+
+    return _filter_serial_or_concole(old) == _filter_serial_or_concole(new)
+
+
+def _diff_serial_list(old, new):
+    """
+        Compare serial definitions to extract the changes
+
+        :param old: list of ElementTree nodes representing the old serials
+        :param new: list of ElementTree nodes representing the new serials
+        """
+    return _diff_lists(old, new, _serial_or_concole_equal)
+
+
+def _diff_console_list(old, new):
+    """
+        Compare console definitions to extract the changes
+
+        :param old: list of ElementTree nodes representing the old consoles
+        :param new: list of ElementTree nodes representing the new consoles
+        """
+    return _diff_lists(old, new, _serial_or_concole_equal)
+
+
 def update(
     name,
     cpu=0,
@@ -2380,6 +2532,8 @@ def update(
     boot=None,
     test=False,
     boot_dev=None,
+    serials=None,
+    consoles=None,
     **kwargs
 ):
     """
@@ -2444,6 +2598,17 @@ def update(
 
         .. versionadded:: Magnesium
 
+    :param serials:
+        Dictionary providing details on the serials connection to create. (Default: ``None``)
+        See :ref:`init-serials-def` for more details on the possible values.
+
+        .. versionadded:: Sodium
+    :param consoles:
+        Dictionary providing details on the consoles device to create. (Default: ``None``)
+        See :ref:`init-consoles-def` for more details on the possible values.
+
+        .. versionadded:: Sodium
+
     :param test: run in dry-run mode if set to True
 
         .. versionadded:: 3001
@@ -2507,6 +2672,9 @@ def update(
             desc.find(".//os/type").get("arch"),
             graphics,
             boot,
+            boot_dev,
+            serials,
+            consoles,
             **kwargs
         )
     )
@@ -2573,6 +2741,8 @@ def update(
         "disk": ["disks", "disk_profile"],
         "interface": ["interfaces", "nic_profile"],
         "graphics": ["graphics"],
+        "serial": ["serial"],
+        "console": ["console"],
     }
     changes = {}
     for dev_type in parameters:

salt/states/virt.py
@@ -288,6 +288,8 @@ def defined(
     boot=None,
     update=True,
     boot_dev=None,
+    serials=None,
+    consoles=None,
 ):
     """
     Starts an existing guest, or defines and starts a new VM with specified arguments.
@@ -356,6 +358,17 @@ def defined(
 
         .. versionadded:: Magnesium
 
+    :param serials:
+        Dictionary providing details on the serials connection to create. (Default: ``None``)
+        See :ref:`init-serials-def` for more details on the possible values.
+
+        .. versionadded:: Sodium
+    :param consoles:
+        Dictionary providing details on the consoles device to create. (Default: ``None``)
+        See :ref:`init-consoles-def` for more details on the possible values.
+
+        .. versionadded:: Sodium
+
     .. rubric:: Example States
 
     Make sure a virtual machine called ``domain_name`` is defined:
@@ -418,6 +431,8 @@ def defined(
                     username=username,
                     password=password,
                     boot=boot,
+                    serials=serials,
+                    consoles=consoles,
                     test=__opts__["test"],
                     boot_dev=boot_dev,
                 )
@@ -453,6 +468,8 @@ def defined(
                     username=username,
                     password=password,
                     boot=boot,
+                    serials=serials,
+                    consoles=consoles,
                     start=False,
                     boot_dev=boot_dev,
                 )
@@ -488,6 +505,8 @@ def running(
     arch=None,
     boot=None,
     boot_dev=None,
+    serials=None,
+    consoles=None,
 ):
     """
     Starts an existing guest, or defines and starts a new VM with specified arguments.
@@ -581,6 +600,16 @@ def running(
         pass a None object, for instance: 'kernel': ``None``.
 
         .. versionadded:: 3000
+    :param serials:
+        Dictionary providing details on the serials connection to create. (Default: ``None``)
+        See :ref:`init-serials-def` for more details on the possible values.
+
+        .. versionadded:: Sodium
+    :param consoles:
+        Dictionary providing details on the consoles device to create. (Default: ``None``)
+        See :ref:`init-consoles-def` for more details on the possible values.
+
+        .. versionadded:: Sodium
 
     :param boot_dev:
         Space separated list of devices to boot from sorted by decreasing priority.
@@ -661,6 +690,8 @@ def running(
         connection=connection,
         username=username,
         password=password,
+        serials=serials,
+        consoles=consoles,
     )
 
     result = True if not __opts__["test"] else None

salt-extensions/community-extensions-holding

salt/modules/virt.py
@@ -184,6 +184,10 @@ VIRT_STATE_NAME_MAP = {
     6: "crashed",
 }
 
+default_port = 23023
+default_serial_type = "tcp"
+default_console_type = "tcp"
+
 
 def __virtual__():
     if not HAS_LIBVIRT:
@@ -946,6 +950,8 @@ def _gen_xml(
     numatune=None,
     hypervisor_features=None,
     clock=None,
+    serials=None,
+    consoles=None,
     **kwargs
 ):
     """
@@ -1021,18 +1027,70 @@ def _gen_xml(
                 context["boot"]["kernel"] = "/usr/lib/grub2/x86_64-xen/grub.xen"
                 context["boot_dev"] = []
 
-    if "serial_type" in kwargs:
-        context["serial_type"] = kwargs["serial_type"]
-    if "serial_type" in context and context["serial_type"] == "tcp":
-        if "telnet_port" in kwargs:
-            context["telnet_port"] = kwargs["telnet_port"]
-        else:
-            context["telnet_port"] = 23023  # FIXME: use random unused port
-    if "serial_type" in context:
-        if "console" in kwargs:
-            context["console"] = kwargs["console"]
-        else:
-            context["console"] = True
+    context["serials"] = []
+    if serials is not None:
+        for serial in serials:
+            serial_type = serial.get("type") or default_serial_type
+            if not serial_type:
+                raise SaltInvocationError("Missing type in serial")
+
+            serial_context = {"type": serial_type}
+            if serial_context["type"] == "tcp":
+                serial_context["port"] = serial.get("port", default_port)
+                serial_context["protocol"] = serial.get("protocol", "telnet")
+            context["serials"].append(serial_context)
+
+    context["consoles"] = []
+    if consoles is not None:
+        for console in consoles:
+            console_type = console.get("type") or default_console_type
+            if not console_type:
+                raise SaltInvocationError("Missing type in console")
+
+            console_context = {"type": console_type}
+            if console_context["type"] == "tcp":
+                console_context["port"] = console.get("port", default_port)
+                console_context["protocol"] = console.get("protocol", "telnet")
+            context["consoles"].append(console_context)
+
+    # processing of deprecated parameters
+    old_port = kwargs.get("telnet_port")
+    if old_port:
+        salt.utils.versions.warn_until(
+            "Aluminium",
+            "'telnet_port' parameter has been deprecated, use the 'serials' and 'consoles' parameters instead. "
+            "It will be removed in {version}.",
+        )
+
+    old_serial_type = kwargs.get("serial_type")
+    if old_serial_type:
+        salt.utils.versions.warn_until(
+            "Aluminium",
+            "'serial_type' parameter has been deprecated, use the 'serials' and 'consoles' parameters instead. "
+            "It will be removed in {version}.",
+        )
+        serial_context = {"type": serial_type}
+        if serial_context["type"] == "tcp":
+            serial_context["port"] = old_port if old_port is not None else default_port
+            serial_context["protocol"] = serial.get("protocol", "telnet")
+        context["serials"].append(serial_context)
+
+    old_console = kwargs.get("console")
+    if old_console:
+        salt.utils.versions.warn_until(
+            "Aluminium",
+            "'console' parameter has been deprecated, use the 'serials' and 'consoles' parameters instead. "
+            "It will be removed in {version}.",
+        )
+        if old_console is True:
+            console_context = {
+                "type": old_serial_type if old_serial_type else default_console_type
+            }
+            if console_context["type"] == "tcp":
+                console_context["port"] = old_port if old_port else default_port
+                console_context["protocol"] = console.get("protocol", "telnet")
+            context["consoles"].append(console_context)
+    # end processing of deprecated parameters
 
     context["disks"] = []
     disk_bus_map = {"virtio": "vd", "xen": "xvd", "fdc": "fd", "ide": "hd"}
@@ -1904,6 +1962,8 @@ def init(
     numatune=None,
     hypervisor_features=None,
     clock=None,
+    serials=None,
+    consoles=None,
     **kwargs
 ):
     """
@@ -2212,6 +2272,18 @@ def init(
                   limit: 2342
                 hpet:
                   present: False
+
+    :param serials:
+        Dictionary providing details on the serials connection to create. (Default: ``None``)
+        See :ref:`init-serials-def` for more details on the possible values.
+
+        .. versionadded:: Aluminium
+
+    :param consoles:
+        Dictionary providing details on the consoles device to create. (Default: ``None``)
+        See :ref:`init-consoles-def` for more details on the possible values.
+
+        .. versionadded:: Aluminium
 
     .. _init-cpu-def:
 
@@ -2585,6 +2657,40 @@ def init(
         By default, not setting the ``listen`` part of the dictionary will default to
         listen on all addresses.
 
+    .. _init-serials-def:
+
+    .. rubric:: Serials Definitions
+
+    Serial dictionaries can contain the following properties:
+
+    type
+        Type of the serial connection, like ``'tcp'``, ``'pty'``.
+
+    port
+        The serial port number.
+        (Default: 23023)
+
+    Protocol
+        Name of connection protocol.
+        (Default: telnet)
+
+    .. _init-consoles-def:
+
+    .. rubric:: Consoles Definitions
+
+    Consol dictionaries can contain the following properties:
+
+    type
+        Type of the serial connection, like ``'tcp'``, ``'pty'``.
+
+    port
+        The serial port number.
+        (Default: 23023)
+
+    Protocol
+        Name of connection protocol.
+        (Default: telnet)
+
     .. rubric:: CLI Example
 
     .. code-block:: bash
@@ -2737,6 +2843,8 @@ def init(
             numatune,
             hypervisor_features,
             clock,
+            serials,
+            consoles,
             **kwargs
         )
         log.debug("New virtual machine definition: %s", vm_xml)
@@ -3007,6 +3115,44 @@ def _normalize_cpusets(desc, data):
             tuning[child] = new_item
 
 
+def _serial_or_concole_equal(old, new):
+    def _filter_serial_or_concole(item):
+        """
+        Filter out elements to ignore when comparing items
+        """
+        return {
+            "type": item.attrib["type"],
+            "port": item.find("source").attrib["service"]
+            if item.find("source") is not None
+            else None,
+            "protocol": item.find("protocol").attrib["type"]
+            if item.find("protocol") is not None
+            else None,
+        }
+
+    return _filter_serial_or_concole(old) == _filter_serial_or_concole(new)
+
+
+def _diff_serial_list(old, new):
+    """
+        Compare serial definitions to extract the changes
+
+        :param old: list of ElementTree nodes representing the old serials
+        :param new: list of ElementTree nodes representing the new serials
+        """
+    return _diff_lists(old, new, _serial_or_concole_equal)
+
+
+def _diff_console_list(old, new):
+    """
+        Compare console definitions to extract the changes
+
+        :param old: list of ElementTree nodes representing the old consoles
+        :param new: list of ElementTree nodes representing the new consoles
+        """
+    return _diff_lists(old, new, _serial_or_concole_equal)
+
+
 def update(
     name,
     cpu=0,
@@ -3023,6 +3169,8 @@ def update(
     boot_dev=None,
     hypervisor_features=None,
     clock=None,
+    serials=None,
+    consoles=None,
     **kwargs
 ):
     """
@@ -3120,6 +3268,18 @@ def update(
         To update any numatune parameters, specify the new value. To remove any ``numatune`` parameters, pass a None object,
         for instance: 'numatune': ``None``. Please note that ``None`` is mapped to ``null`` in sls file, pass ``null`` in
         sls file instead.
+
+        .. versionadded:: Aluminium
+
+    :param serials:
+        Dictionary providing details on the serials connection to create. (Default: ``None``)
+        See :ref:`init-serials-def` for more details on the possible values.
+
+        .. versionadded:: Aluminium
+
+    :param consoles:
+        Dictionary providing details on the consoles device to create. (Default: ``None``)
+        See :ref:`init-consoles-def` for more details on the possible values.
 
         .. versionadded:: Aluminium
 
@@ -3248,6 +3408,9 @@ def update(
             desc.find(".//os/type").get("arch"),
             graphics,
             boot,
+            boot_dev,
+            serial=serials,
+            consoles=consoles,
             **kwargs
         )
     )
@@ -3632,6 +3795,8 @@ def update(
         "disk": ["disks", "disk_profile"],
         "interface": ["interfaces", "nic_profile"],
         "graphics": ["graphics"],
+        "serial": ["serial"],
+        "console": ["console"],
     }
     changes = {}
     for dev_type in parameters:

salt/states/virt.py
@@ -289,6 +289,8 @@ def defined(
     boot_dev=None,
     hypervisor_features=None,
     clock=None,
+    serials=None,
+    consoles=None,
 ):
     """
     Starts an existing guest, or defines and starts a new VM with specified arguments.
@@ -563,6 +565,17 @@ def defined(
             clock:
               timezone: CEST
 
+    :param serials:
+        Dictionary providing details on the serials connection to create. (Default: ``None``)
+        See :ref:`init-serials-def` for more details on the possible values.
+
+        .. versionadded:: Aluminium
+    :param consoles:
+        Dictionary providing details on the consoles device to create. (Default: ``None``)
+        See :ref:`init-consoles-def` for more details on the possible values.
+
+        .. versionadded:: Aluminium
+
     .. rubric:: Example States
 
     Make sure a virtual machine called ``domain_name`` is defined:
@@ -626,6 +639,8 @@ def defined(
                     password=password,
                     boot=boot,
                     numatune=numatune,
+                    serials=serials,
+                    consoles=consoles,
                     test=__opts__["test"],
                     boot_dev=boot_dev,
                     hypervisor_features=hypervisor_features,
@@ -664,6 +679,8 @@ def defined(
                     password=password,
                     boot=boot,
                     numatune=numatune,
+                    serials=serials,
+                    consoles=consoles,
                     start=False,
                     boot_dev=boot_dev,
                     hypervisor_features=hypervisor_features,
@@ -704,6 +721,8 @@ def running(
     numatune=None,
     hypervisor_features=None,
     clock=None,
+    serials=None,
+    consoles=None,
 ):
     """
     Starts an existing guest, or defines and starts a new VM with specified arguments.
@@ -820,6 +839,16 @@ def running(
         pass a None object, for instance: 'kernel': ``None``.
 
         .. versionadded:: 3000
+    :param serials:
+        Dictionary providing details on the serials connection to create. (Default: ``None``)
+        See :ref:`init-serials-def` for more details on the possible values.
+
+        .. versionadded:: Aluminium
+    :param consoles:
+        Dictionary providing details on the consoles device to create. (Default: ``None``)
+        See :ref:`init-consoles-def` for more details on the possible values.
+
+        .. versionadded:: Aluminium
 
     :param boot_dev:
         Space separated list of devices to boot from sorted by decreasing priority.
@@ -972,6 +1001,8 @@ def running(
         connection=connection,
         username=username,
         password=password,
+        serials=serials,
+        consoles=consoles,
     )
 
     result = True if not __opts__["test"] else None